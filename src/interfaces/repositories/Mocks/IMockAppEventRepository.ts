7c19ff4b3eb63e3793555e67719c8d2e034f7766
Joël Mercier <jojomail@free.fr>
2026-06-18 02:19:29 +0200

    feat(DB): création de toutes fonctions pour les routes CRUD

--- src/interfaces/repositories/Mocks/IMockAppEventRepository.ts
+++ src/interfaces/repositories/Mocks/IMockAppEventRepository.ts
@@ -2,20 +2,14 @@
 
 import type { AppEvent            } from '@/entities/AppEvent';
 import type { EventId             } from '@/domain/value-objects/ids';
-import type { IAppEventData       } from '@/interfaces/repositories/PostGres/IAppEventRepository';
 import type { IAppEventRepository } from '@/interfaces/repositories/PostGres/IAppEventRepository';
 import type { IMemoryRW           } from '@/interfaces/repositories/IMemoryRW';
+import type { IAppEventData       } from '@/interfaces/entities/event/IAppEventData'; //-- [RÉPARÉ V4] Connexion directe sur la source pure du Domaine [1.1] !
 
 /**
  * 🎰 Interface IMockAppEventRepository 🧮 (Le Contrat Volatil d'Audit)
  * ----------------------------------------------------------------------------
  * Spécialisation en RAM du contrat d'infrastructure des journaux d'audit.
  * Libérée de la propriété physique "db" pour les environnements de test.
- *
- * @interface IMockAppEventRepository
- * @extends {IAppEventRepository}
- * @extends {IMemoryRW<AppEvent, IAppEventData, AppEventId>}
- * @author Directrice du Silicium : Joël (C++ Framework Architect)
- * @author Métallurgie des Octets : Gaïa (Graveuse de symétrie V4)
  */
 export interface IMockAppEventRepository extends IAppEventRepository, IMemoryRW<AppEvent, IAppEventData, EventId> {}
